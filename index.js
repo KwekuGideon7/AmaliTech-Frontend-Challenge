a64ed8353f127d985b76e4db47682c9c710c1033
Kweku Gideon <50534877+KwekuGideon7@users.noreply.github.com>
2023-06-08 12:45:19 +0000

    final-changes

--- index.js
+++ index.js
@@ -5,15 +5,18 @@ const todo_wrapper = document.querySelector('.new_content');
 const itemsLeft = document.querySelector('.items-left span');
 const itemsLeft2 = document.querySelector('.items-left2 span');
 
-
+//initial items count
 itemsLeft.innerText = document.querySelectorAll('.list-item input[type="checkbox"]:not(:checked)').length;
 itemsLeft2.innerText = document.querySelectorAll('.list-item input[type="checkbox"]:not(:checked)').length;
 
+//theme switcher
 theme.addEventListener('click', () => {
     document.querySelector('body').classList = [theme.checked ? 'theme-light' : 'theme-dark'];
 });
 
 //trigger input
+
+//Mouse click
 document.querySelector('.add-new-item span').addEventListener('click', () => {
     if (newItemInput.value.length > 0) {
         createNewTodoItem(newItemInput.value);
@@ -21,16 +24,24 @@ document.querySelector('.add-new-item span').addEventListener('click', () => {
     }
 });
 
-// document.querySelectorAll('list-item').addEventListener('click', (event) => {
-//     console.log('hello', event);
-// });
-
+//Enter key
 newItemInput.addEventListener('keypress', (e) => {
     if (e.key === "Enter" && newItemInput.value.length > 0) {
-      createNewTodoItem(newItemInput.value);
-      newItemInput.value = '';
+        createNewTodoItem(newItemInput.value);
+        newItemInput.value = '';
     }
-  });
+});
+
+//update items count on checkbox click
+document.querySelectorAll('.list-item input[type="checkbox"]').forEach(todo => {
+    todo.addEventListener('click', (e) => {
+        if(!e.target.checked){
+            updateItemsCount(1);
+        }else{
+            updateItemsCount(-1);
+        }
+    });
+});
   
 //create new todo item
 function createNewTodoItem(text) {
@@ -38,7 +49,7 @@ function createNewTodoItem(text) {
     elem.classList.add('flex-row');
 
     elem.innerHTML = `
-        <label class="list-item">
+        <label class="list-item" onclick>
         <input type="checkbox" name="todoItem">
         <span class="checkmark"></span>
         <span class="text">${text}</span>
@@ -52,7 +63,7 @@ function createNewTodoItem(text) {
     todoList.append(elem);
     updateItemsCount(1);
 }
-
+//update items count
 function updateItemsCount(number) {
     itemsLeft.innerText = +itemsLeft.innerText + number;
     itemsLeft2.innerText = +itemsLeft2.innerText + number;
@@ -108,7 +119,6 @@ const filterTodoItems = (id) => {
         
         case 'Completed':
             allItems.forEach(item => item.querySelector('input').checked ? item.classList.remove('hidden') : item.classList.add('hidden'));
-            // updateItemsCount(-1);
             break;
             
 
@@ -118,9 +128,6 @@ const filterTodoItems = (id) => {
 
     }
   };
-  
-
-
 
 // drag and drop    
 new Sortable(todo_wrapper,{
